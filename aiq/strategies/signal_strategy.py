e0549b14b3bbd005cec724984b56e00c50ed3c4c
boundles <wangyang9113@gmail.com>
2023-02-19 18:06:35 +0800

    use topk still

--- aiq/strategies/signal_strategy.py
+++ aiq/strategies/signal_strategy.py
@@ -107,7 +107,7 @@ class TopkDropoutStrategy(bt.Strategy):
         # re-balance those already top ranked and still there
         for secu in keep_order_list:
             data = self.getdatabyname(secu)
-            order_value = self.broker.getvalue() * (1 - self.reserve) / len(self.current_stock_list)
+            order_value = self.broker.getvalue() * (1 - self.reserve) / self.p.topk
             order_amount = self.downcast(order_value / data.close[0], 100)
             self.order_target_size(data, target=order_amount)
 
@@ -115,7 +115,7 @@ class TopkDropoutStrategy(bt.Strategy):
         # do this last, as this will generate buy orders consuming cash
         for secu in buy_order_list:
             data = self.getdatabyname(secu)
-            order_value = self.broker.getvalue() * (1 - self.reserve) / len(self.current_stock_list)
+            order_value = self.broker.getvalue() * (1 - self.reserve) / self.p.topk
             order_amount = self.downcast(order_value / data.close[0], 100)
             self.buy(data, size=order_amount, name=secu)
 
