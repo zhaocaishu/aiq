4f49f383e7aa581a542b89a86bea242eb5b680d1
boundles <wangyang9113@gmail.com>
2023-03-01 19:08:54 +0800

    fix bug

--- tools/get_topk_indicators.py
+++ tools/get_topk_indicators.py
@@ -8,6 +8,7 @@ from aiq.utils.config import config as cfg
 def parse_args():
     parser = argparse.ArgumentParser(description='Feature importance')
     parser.add_argument('--save_dir', type=str, help='the saved directory')
+    parser.add_argument('--model_type', type=str, default='XGB', help='model type')
     parser.add_argument('--topk', type=int, default=50, help='top k')
 
     args = parser.parse_args()
@@ -20,7 +21,10 @@ def main():
     args = parse_args()
 
     # model
-    model = LGBModel()
+    if args.model_type == 'LGB':
+        model = LGBModel()
+    elif args.model_type == 'XGB':
+        model = XGBModel()
     model.load(args.save_dir)
 
     # feature importance
@@ -30,7 +34,11 @@ def main():
     for row in feature_importance.items():
         if count >= args.topk:
             break
-        topk_indicators.append(model.feature_cols[row[0]])
+        if args.model_type == 'XGB':
+            feature_index = int(row[0].replace('f', ''))
+        else:
+            feature_index = int(row[0])
+        topk_indicators.append(model.feature_cols[feature_index])
         count += 1
 
     print(','.join(topk_indicators))
