9333f02d28ddda172b58ec6de9ad01fc8b6876fa
boundles <wangyang9113@gmail.com>
2023-03-02 20:27:20 +0800

    fix bug

--- aiq/strategies/signal_strategy.py
+++ aiq/strategies/signal_strategy.py
@@ -129,11 +129,13 @@ class TopkDropoutStrategy(bt.Strategy):
             if current_value < target_value:
                 order_price = data.open[1]
                 order_size = self.downcast((target_value - current_value) / order_price, 100)
-                self.order[secu] = self.buy(data=data, size=order_size, price=order_price, name=secu)
+                if order_size > 0:
+                    self.order[secu] = self.buy(data=data, size=order_size, price=order_price, name=secu)
             elif current_value > target_value:
                 order_price = data.close[1]
                 order_size = self.downcast((current_value - target_value) / order_price, 100)
-                self.order[secu] = self.sell(data=data, size=order_size, price=order_price, name=secu)
+                if order_size > 0:
+                    self.order[secu] = self.sell(data=data, size=order_size, price=order_price, name=secu)
 
         # issue a target order for the newly top ranked stocks
         # do this last, as this will generate buy orders consuming cash
