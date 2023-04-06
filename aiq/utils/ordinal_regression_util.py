270dcf30efd4f0c76360d8030f3aacc7b055595a
boundles <wangyang9113@gmail.com>
2023-04-06 23:32:07 +0800

    fix

--- aiq/utils/ordinal_regression_util.py
+++ aiq/utils/ordinal_regression_util.py
@@ -4,7 +4,7 @@ import numpy as np
 def reg2multilabel(logits, min=-0.1, step=0.01, num_classes=20):
     labels = np.zeros((logits.shape[0], num_classes), dtype=np.int32)
     for i, logit in enumerate(logits):
-        labels[i, 0:int((logit[0] - min) / step)+1] = 1
+        labels[i, 0:int((logit - min) / step)+1] = 1
     return labels
 
 
