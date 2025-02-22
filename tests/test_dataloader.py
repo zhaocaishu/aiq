ad720cd6533ef82ae0f8aa4de633596f00797f50
boundles <wangyang9113@gmail.com>
2025-02-22 13:23:13 +0800

    fix typo

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -2,10 +2,10 @@ from aiq.dataset.loader import DataLoader
 
 
 if __name__ == "__main__":
-    df = DataLoader.load_instruments(
+    instruments = DataLoader.load_instruments(
         data_dir=None,
         market="000300.SH",
         start_time="2025-01-01",
         end_time="2025-01-03",
     )
-    print(df)
+    print(instruments)
