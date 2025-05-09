300c25cde6f6aaecfa3ad53a620aa014641b0c52
DarrenWang <wangyang9113@gmail.com>
2025-05-09 15:22:58 +0800

    Update handler.py

--- aiq/dataset/handler.py
+++ aiq/dataset/handler.py
@@ -335,8 +335,8 @@ class Alpha158(DataHandler):
             # The volume weighted price change volatility
             for d in windows:
                 features.append(
-                    Std(Abs(close / Ref(close, 1) - 1) * volume, d)
-                    / (Mean(Abs(close / Ref(close, 1) - 1) * volume, d) + 1e-12)
+                    Std((close / Ref(close, 1) - 1) * Log(1 + volume), d)
+                    / (Mean((close / Ref(close, 1) - 1) *  Log(1 + volume), d) + 1e-12)
                 )
                 feature_names.append("WVMA%d" % d)
 
