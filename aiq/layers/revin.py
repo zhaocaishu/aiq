1af4021c5a0336b9ecc8ac2e1b729ac4d4e09a78
DarrenWang <wangyang9113@gmail.com>
2025-05-29 14:00:01 +0800

    Update revin.py

--- aiq/layers/revin.py
+++ aiq/layers/revin.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class RevIN(nn.Module):
-    def __init__(self, num_features: int, eps=1e-5, affine=True):
+    def __init__(self, num_features: int, eps=1e-5, affine=False):
         """
         :param num_features: the number of features or channels
         :param eps: a value added for numerical stability
