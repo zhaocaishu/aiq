12e2577c170fcc1a2b9ee51dc35e8ba68156c15c
DarrenWang <wangyang9113@gmail.com>
2025-06-27 15:18:45 +0800

    Update __init__.py

--- aiq/losses/__init__.py
+++ aiq/losses/__init__.py
@@ -1,5 +1,6 @@
 from .cb_loss import ClassBalancedLoss
 from .mr_loss import MSERankLoss
+from .ic_loss import ICLoss
 
 
-__all__ = ["ClassBalancedLoss", "MSERankLoss"]
+__all__ = ["ClassBalancedLoss", "MSERankLoss", "ICLoss"]
