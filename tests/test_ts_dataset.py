1cdc6403543ac614865e5bff354f454f77566dc4
DarrenWang <wangyang9113@gmail.com>
2025-07-07 14:23:33 +0800

    Update test_ts_dataset.py

--- tests/test_ts_dataset.py
+++ tests/test_ts_dataset.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         feature_names=data_handler.feature_names,
         label_names=["RETN_5D"],
         use_augmentation=True,
-        augment_start_index=7,
+        augmentation_feature_start_index=7,
         mode="train",
     )
 
