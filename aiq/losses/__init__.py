0aa545002aad88713d29d8169367fa2cdc0d96e7
boundles <wangyang9113@gmail.com>
2025-11-08 20:01:25 +0800

    modify loss

--- aiq/losses/__init__.py
+++ aiq/losses/__init__.py
@@ -1,6 +1,13 @@
-from .cb_loss import ClassBalancedLoss
+from .mr_loss import MarginRankingLoss
 from .ic_loss import ICLoss
 from .topk_loss import TopKLoss
 from .fuse_loss import FuseLoss
+from .cb_loss import ClassBalancedLoss
 
-__all__ = ["ClassBalancedLoss", "ICLoss", "TopKLoss", "FuseLoss"]
+__all__ = [
+    "MarginRankingLoss",
+    "ICLoss",
+    "TopKLoss",
+    "FuseLoss",
+    "ClassBalancedLoss",
+]
