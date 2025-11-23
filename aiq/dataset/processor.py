99f9050fc8fa440a3ca30dc615915d0bc55d4ec4
boundles <wangyang9113@gmail.com>
2025-11-23 21:14:21 +0800

    fix

--- aiq/dataset/processor.py
+++ aiq/dataset/processor.py
@@ -141,7 +141,7 @@ class RobustZScoreNorm(Processor):
 class CSNeutralize(Processor):
     """Factors Neutralization"""
 
-    def __init__(self, industry_col: str, cap_col: Optional[str], factor_cols: list):
+    def __init__(self, industry_col: str, cap_col: Optional[str], factor_cols: List[str]):
         self.industry_col = industry_col
         self.cap_col = cap_col
         self.factor_cols = factor_cols
